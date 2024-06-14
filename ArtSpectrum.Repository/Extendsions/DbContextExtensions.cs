1f8abcf32ee7742d08f121dadaeea6e6e1994afb
nguyenthanhdat1234 <datnguyen.010802@gmail.com>
2024-06-14 12:40:50 +0700

    add fix get descending

---
+++ ArtSpectrum.Repository/Extendsions/DbContextExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace ArtSpectrum.Extendsions
+{
+    public static class DbContextExtensions
+    {
+        public static IQueryable<T> OrderByIdDescending<T>(this DbSet<T> dbSet) where T : class
+        {
+            var parameter = Expression.Parameter(typeof(T), "e");
+            var property = typeof(T).GetProperties()
+                                    .FirstOrDefault(p => p.Name.ToLower().Contains("id"));
+
+            if (property == null)
+            {
+                throw new InvalidOperationException("No ID property found for type " + typeof(T).Name);
+            }
+
+            var propertyAccess = Expression.MakeMemberAccess(parameter, property);
+            var orderByExp = Expression.Lambda(propertyAccess, parameter);
+            var resultExp = Expression.Call(
+                typeof(Queryable),
+                "OrderByDescending",
+                new Type[] { typeof(T), property.PropertyType },
+                dbSet.AsQueryable().Expression,
+                Expression.Quote(orderByExp)
+            );
+
+            return dbSet.AsQueryable().Provider.CreateQuery<T>(resultExp);
+        }
+    }
+}
