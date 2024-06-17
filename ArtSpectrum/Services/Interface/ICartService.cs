2212974362d917eb3bb96db78beea254955264eb
nguyenthanhdat1234 <datnguyen.010802@gmail.com>
2024-06-17 17:10:30 +0700

    fix get by id to cart

--- ArtSpectrum/Services/Interface/ICartService.cs
+++ ArtSpectrum/Services/Interface/ICartService.cs
@@ -10,7 +10,7 @@ namespace ArtSpectrum.Services.Interface
         Task<List<ResponseCart>> CreateCartAsync(CreateCartRequest request, CancellationToken cancellationToken);
         Task<List<CartDto>> GetAll();
 
-        Task<List<CartDto>> GetCartByIdAsync(int cartId, CancellationToken cancellationToken);
+        Task<List<ResponseCart>> GetCartByIdAsync(int cartId, CancellationToken cancellationToken);
         Task<CartDto> UpdateCartAsync(int cartId, UpdateCartRequest request, CancellationToken cancellationToken);
         Task<CartDto> DeleteCartByIdAsync(int cartId, CancellationToken cancellationToken);
     }
