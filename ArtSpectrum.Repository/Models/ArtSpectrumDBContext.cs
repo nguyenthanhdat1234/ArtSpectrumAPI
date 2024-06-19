be55f0d89302db6c7be3be59ae66fb0f3c7f9296
tranbinhminh2110 <tranbinhminh211002@gmail.com>
2024-06-19 13:29:52 +0700

    add cartId

--- ArtSpectrum.Repository/Models/ArtSpectrumDBContext.cs
+++ ArtSpectrum.Repository/Models/ArtSpectrumDBContext.cs
@@ -29,14 +29,14 @@ namespace ArtSpectrum.Repository.Models
         public virtual DbSet<Sale> Sales { get; set; } = null!;
         public virtual DbSet<User> Users { get; set; } = null!;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        /*protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Data Source=DESKTOP-HHD16EI;Initial Catalog=ArtSpectrumDB;Integrated Security=True;Trust Server Certificate=True");
             }
-        }
+        }*/
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
